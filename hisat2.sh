a74269ecabfdfb9e4b9f4b9ac97310ee086505dd
Antrelle D. Clark <167575514+AClark98@users.noreply.github.com>
2024-09-11 12:06:35 -0500

    Update hisat2.sh

--- hisat2.sh
+++ hisat2.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
-
-
+#
 # Load the module
+#load the GNU compiling software environment
 source /apps/profiles/modules_asax.sh.dyn
 module load gcc/9.3.0
+what is the command line?
+
+#!/bin/sh
+#
+#load the HISAT2 alignment program for mapping ngs environment
 module load hisat2/2.2.0
+what is the command line?
+
+#!/bin/sh
+#
+#load the SAMtools environment
+source /apps/profiles/modules_asax.sh.dyn
 module load samtools/1.18
+what is the command line?
 
 # This first step involves locating splice sites and exons within your genome. It requires that your genome has an annotation, which must be in GTF format. 
 # Most genomes that have come out recently will have their annotation information in GFF format, which is slightly different. You can convert between GFF and GTF format using the program gffread. There's great tutorials for this on the internet!
 
 # Identify your splice sites by running:
-# hisat2_extract_expns.py F_heteroclitus_genannotation.gtf > splice_sites.hisat
+# hisat2_extract_exons.py F_heteroclitus_genannotation.gtf > splice_sites.hisat
 
 # Identify your exons by running:
 # hisat2_extract_exons.py F_heteroclitus_genannotation.gtf > exons.hisat
