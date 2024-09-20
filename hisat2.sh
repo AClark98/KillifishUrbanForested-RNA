63d09b6fddc1ebdeb5fa5c9fd577fe9565b85d77
Antrelle D. Clark <167575514+AClark98@users.noreply.github.com>
2024-09-20 10:32:22 -0500

    Update hisat2.sh

--- hisat2.sh
+++ hisat2.sh
@@ -4,7 +4,7 @@
 source /apps/profiles/modules_asax.sh.dyn
 # module load hisat2/2.2.0
 # module load samtools/1.18
-^save as hisat2.sh
+#^save as hisat2.sh
 
 # This first step involves locating splice sites and exons within your genome. It requires that your genome has an annotation, which must be in GTF format. 
 # Most genomes that have come out recently will have their annotation information in GFF format, which is slightly different. You can convert between GFF and GTF format using the program gffread. There's great tutorials for this on the internet!
